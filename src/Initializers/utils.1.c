a05b9e23db275b4e8a1df58c9be0bab8745f8499
PedroVMota <pedromvmota@outlook.pt>
2024-02-05 21:36:49 +0000

    Norminette

--- src/Initializers/utils.1.c
+++ src/Initializers/utils.1.c
@@ -1,50 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 21:32:45 by pedro             #+#    #+#             */
+/*   Updated: 2024/02/05 21:35:33 by pedro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <center.h>
 
-static void *err(gscene **main)
+bool	initialize_mlx(gscene *s)
 {
-    if(!*main)
-        return NULL;
-    free(*main);
-    *main = NULL;
-    return (void *)*main;
+	s->mlx->mlx = mlx_init();
+	if (!s->mlx->mlx)
+		return (false);
+	s->mlx->win = mlx_new_window(s->mlx->mlx, s->width, s->height, "miniRT");
+	if (!s->mlx->win)
+		return (false);
+	s->mlx->img = mlx_new_image(s->mlx->mlx, s->width, s->height);
+	if (!s->mlx->img)
+		return (false);
+	s->mlx->addr = mlx_get_data_addr(s->mlx->img, &s->mlx->bits_per_pixel, \
+		&s->mlx->line_length, &s->mlx->endian);
+	if (!s->mlx->addr)
+		return (false);
+	return (true);
 }
 
-bool	initialize_mlx(gscene *s){
-    s->mlx->mlx = mlx_init();
-    if(!s->mlx->mlx)
-        return (false);
-    s->mlx->win = mlx_new_window(s->mlx->mlx, s->width,s->height, "miniRT");
-    if(!s->mlx->win)
-        return (false);
-    s->mlx->img = mlx_new_image(s->mlx->mlx, s->width,s->height);
-    if(!s->mlx->img)
-        return (false);
-    s->mlx->addr = mlx_get_data_addr(s->mlx->img, &s->mlx->bits_per_pixel,
-                                         &s->mlx->line_length, &s->mlx->endian);
-    if(!s->mlx->addr)
-        return (false);
-    return (true);
-}
-
-gscene *init_MainStruct(int width, int height)
+gscene	*init_main(int width, int height)
 {
-    gscene *m;
+	gscene	*m;
 
-    m = NULL;
-    printf("Sizeof %lu\n\n\n", sizeof(gscene));
-    m = malloc(sizeof(gscene));
-    if(!m)
-        return NULL;
-    m->width = width;
-    m->height = height;
-    m->camera = NULL;
-    m->objects = NULL;
-    m->lights = NULL;
-    m->mlx = malloc(sizeof(t_mlxdata));
-    if (!m->mlx)
-        return (gscene *)err(&m);
-    if(!initialize_mlx(m))
-        return (gscene *)err(&m);
-    return m;
+	m = malloc(sizeof(gscene));
+	if (!m)
+		return (NULL);
+	m->width = width;
+	m->height = height;
+	m->camera = NULL;
+	m->objects = NULL;
+	m->lights = NULL;
+	m->mlx = malloc(sizeof(t_mlxdata));
+	if (!m->mlx)
+		return ((gscene *)err(&m));
+	if (!initialize_mlx(m))
+		return ((gscene *)err(&m));
+	return (m);
 }
-
